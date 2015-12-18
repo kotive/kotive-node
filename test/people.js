5e34c41fe2d86f3d363ae8a27652c6805dba200c
the-djdj <jenkins.daniel.02@gmail.com>
2015-12-18 14:51:17 +0200

    feat: Added Create a Person to API

--- test/people.js
+++ test/people.js
@@ -11,4 +11,12 @@ describe('people', () => {
       done();
     });
   });
+  it('create a new person', done => {
+    nock('https://webapp.kotive.com/api').post('/people', {person: {email: 'jenkins.daniel.02@gmail.com', username: 'jenkins.daniel.02@gmail.com', firstname: 'Daniel', lastname: 'Jenkins', mobile: '', password: '123456', groupId: '246'}}).basicAuth({user: 'jenkins.daniel.02@gmail.com', pass: '123456'}).reply(200);
+    const client = new Client('jenkins.daniel.02@gmail.com', '123456').usePromises();
+    client.people.create({person: {email: 'jenkins.daniel.02@gmail.com', username: 'jenkins.daniel.02@gmail.com', firstname: 'Daniel', lastname: 'Jenkins', mobile: '', password: '123456', groupId: '246'}}).then(r => {
+      assert.equal(200, r.status);
+      done();
+    });
+  });
 });
